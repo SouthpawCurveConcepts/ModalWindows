9bfcd80df8a143f30ff704e2619b2f62b3229f6a
Jay For Short <jagoblacksky@gmail.com>
2021-08-02 01:44:16 -0500

    Lesson 80: Working with classes

--- starter/script.js
+++ starter/script.js
@@ -1,3 +1,18 @@
 'use strict';
 
 console.log('Hello!');
+
+// Todo: Write the code for lesson 79...
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsShowModal = document.querySelectorAll('.show-modal');
+
+console.log(btnsShowModal);
+
+for (let i = 0; i < btnsShowModal.length; i++)
+  btnsShowModal[i].addEventListener('click', function () {
+    console.log('Button clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
